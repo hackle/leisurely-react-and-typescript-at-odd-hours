6c2666dda0b7eb469ebe6bf5581715bef700f840
Hacke Wayne <hacklew@hotmail.com>
2021-07-06 20:12:47 +1200

    add sourcemaps

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,19 @@ import { Provider } from 'react-redux';
 import { createStoreWithApiConfig } from './store';
 import { apiConfig } from './apiConfig';
 
+import * as Sentry from '@sentry/react';
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://aa0edea63055490f9f55e6cbe7fc9a7b@o913027.ingest.sentry.io/5850497",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 const store = createStoreWithApiConfig(apiConfig);
 
 ReactDOM.render(
